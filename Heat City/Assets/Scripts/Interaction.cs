8b87780af883e06eb3817b30bb0b8cd71a4f033c
BejPastan <szymon.synaszko@tm1.edu.pl>
2023-04-15 12:35:44 +0200

    Items
    Picking up, putting down and interaction with world

---
+++ Heat City/Assets/Scripts/Interaction.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField]
+    string newItemName;
+    [SerializeField]
+    PickUpItem itemController;
+    [SerializeField]
+    string needItem;
+
+    [SerializeField]
+    Vector3 newItemPosition;
+
+    [SerializeField]
+    bool dropItem;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "PickUpArea")
+        {
+            itemController = other.transform.GetComponent<PickUpItem>();
+        }
+    }
+
+    public void Interact()
+    {
+        if (itemController.item.name == needItem)
+        {
+            itemController.item.name = newItemName;
+            if (dropItem)
+            {
+                itemController.DropItem(newItemPosition);
+            }
+        }
+        //od³o¿enie przedmiotu lub zmiana jego nazwy
+        
+    }
+}
---
+++ Heat City/Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PickUpItem : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode pickUpKey;
+
+    private bool holdItem;
+    [SerializeField]
+    private Transform hands;
+    private Transform toPickUp;
+    public Transform item;
+
+    [SerializeField]
+    Interaction interaction;
+
+    //gracz naciska przycisk do podnoszenia przedmiotu
+    private void Update()
+    {
+        if (Input.GetKeyDown(pickUpKey))
+        {
+            //gra sprawdza czy gracz nie trzyma przedmiotu
+            if(holdItem)
+            {
+                //sprawdza czy nie jest w miejscu z interakcj¹
+                if(interaction != null)
+                {
+                    //interakcja
+                    interaction.Interact();
+                }
+                else
+                {
+                    //jeœli trzyma przedmiot to puszcza trzymany
+                    DropItem(item.position);
+                }
+            }
+            //jeœli nie trzyma
+            else
+            {
+                //gra sprawdza czy przed graczem jest przedmiot do podniesienia
+                if (toPickUp != null)
+                {
+                    item = toPickUp;
+                    item.SetParent(hands);
+                    item.transform.localPosition = new Vector3(0, -0.25f, 0.5f);
+                    holdItem = true;
+                    //tutaj zmieñmy jeszcze animacjê na chodzenie z itemem ale to nie teraz, teraz to mi siê nie chce
+                }
+            }
+        }
+    }
+
+    public void DropItem(Vector3 position)
+    {
+        item.SetParent(null);
+        item.position = position;
+        item = null;
+        holdItem = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickable")
+        {
+            Debug.Log(other);
+            toPickUp = other.transform;
+        }
+        if(other.GetComponent<Interaction>()!=null)
+        {
+            interaction = other.GetComponent<Interaction>();
+        }
+    }
+
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.transform == toPickUp)
+        {
+            Debug.Log("ju¿ nie");
+            toPickUp = null;
+        }
+        if(other.GetComponent<Interaction>() != null)
+        {
+            interaction = null;
+        }
+    }
+}
